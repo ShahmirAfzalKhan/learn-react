c60eab69730b9c9946282b0171377d9f6aff9df4
ShahmirAfzalKhan <67229565+ShahmirAfzalKhan@users.noreply.github.com>
2024-09-15 17:11:50 +0500

    Add files via upload

---
+++ Vid 12/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header.js'
+import Footer from './components/Footer/Footer.js'
+import Home from './components//Home/Home.js'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      
+    </>
+  )
+}
+
+export default App
